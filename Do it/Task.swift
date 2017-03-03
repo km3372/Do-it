cf38e6473ebb133600988f280418118cc2e2fd37
Ken Moody <km3372@gmail.com>
2017-03-02 19:14:20 -0500

    no message

---
+++ Do it/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  Do it
+//
+//  Created by Ken Moody on 3/2/17.
+//  Copyright © 2017 Ken Moody. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
